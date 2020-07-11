6ca9fac413f3b82f961e66966e7d988482b7a887
apxwn <apxwn@yandex.ru>
2020-07-11 11:31:11 +0200

    catch reference to non-existing datafile, cleanup HTML output

--- datahook-extern.php
+++ datahook-extern.php
@@ -6,32 +6,41 @@ $strJsonFileContents = file_get_contents($filename);
 $data = json_decode($strJsonFileContents, true);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="de">
 <head>
+  <title>Ergebnisse für <?php echo $data['region']; ?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="datahook-extern.css">
 </head>
-<body >
+<body>
   <div class="py-5">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <p class="lead">Hallo <?php echo $data['rufbname'] . ", die Suche in der <strong>Region " . $data['region']; ?>
+          <p class="lead">
+          	<?php if (!file_exists($filename)) { echo ("Falsche Parameter übergeben, diese Daten existieren nicht.</p></div></div></div></div></body>"); die; }
+          	?>
+          	Hallo <?php echo $data['rufbname'] . ", die Suche in der <strong>Region " . $data['region']; ?>
           </strong> ergab Folgendes:</p>
         </div>
 
 <?php
 foreach ($data['ergebnisse'] as $key => $ergebnis) {
-  $output = '<div class="col-md-12"><div class="table-responsive"><table class="table table-bordered "><thead class="thead-dark"><tr><th style="width: 20%;"><strong>' . strtoupper($ergebnis['zeilensprache']) . '</strong></th><th>' . $ergebnis['name'] . '</th></tr></thead>';
-  $output .= '<tbody><tr><th>Telefon</th><td><a href="tel:' . preg_replace('/[^0-9+]+/', '', $ergebnis['telefon']) . '">' . $ergebnis['telefon'] . '</td></tr>';
-  $output .= '<tbody><tr><th>Mobil</th><td><a href="tel:' . preg_replace('/[^0-9+]+/', '', $ergebnis['mobil']) . '">' . $ergebnis['mobil'] . '</td></tr>';
-  $output .= '<tr><th>Aktuelles</th><td><mark>' . $ergebnis['aktuelles'] . '</mark></td></tr>';
-  $output .= '<tr><th scope="row">Bemerkungen</th><td>' . $ergebnis['bemerkungen'] . '</td></tr></tbody></table></div></div>';
+  $output = '<div class="col-md-12"><div class="table-responsive"><table class="table table-bordered "><thead class="thead-dark"><tr><th style="width: 20%;" scope="col"><strong>' . strtoupper($ergebnis['zeilensprache']) . '</strong></th><th scope="col">' . $ergebnis['name'] . '</th></tr></thead>';
+  $output .= '<tbody><tr><th scope="row">Telefon</th><td><a href="tel:' . preg_replace('/[^0-9+]+/', '', $ergebnis['telefon']) . '">' . $ergebnis['telefon'] . '</a></td></tr>';
+  $output .= '<tbody><tr><th scope="row">Mobil</th><td><a href="tel:' . preg_replace('/[^0-9+]+/', '', $ergebnis['mobil']) . '">' . $ergebnis['mobil'] . '</a></td></tr>';
+  $output .= '<tr><th scope="row">Aktuelles</th><td>';
+  	if ($ergebnis['aktuelles']) {
+  		$output .= '<mark>' . $ergebnis['aktuelles'] . '</mark>';
+  	}
+  $output .= '</td></tr><tr><th scope="row">Bemerkungen</th><td>' . $ergebnis['bemerkungen'] . '</td></tr></tbody></table></div></div>';
   echo $output;
 }
 ?>
+  	</div>
   </div>
+</div>
 </body>
 
 </html>
